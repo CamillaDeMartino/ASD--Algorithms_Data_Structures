cf41bda376a2ea7327bb501a919800f2a679bc09
Camilla <camillademartino97@gmail.com>
2022-10-11 22:32:38 +0200

    commit

--- ES. DiveEtImpera/main.cpp
+++ ES. DiveEtImpera/main.cpp
@@ -18,22 +18,25 @@ int Ricerca(int A[], int low, int high)
 {
     int mid, ind;
 
-    if(high - low == 2 && low < high)
-        return low;
-    else if (high - low == 1)
-        return -1;
+    if(high - low == 1)
+    {
+        if (A[low] < A[high])
+            return low;
+        else
+            return -1;
+    }
     else 
-        {
-            mid = (high+low)/2;
-            if(A[mid] < A[mid+1])
-                return mid;
+    {
+        mid = (high+low)/2;
+        if(A[mid] < A[mid+1])
+            return mid;
+        else 
+            ind = Ricerca(A,low,mid);
+            if(ind < 0)
+                return Ricerca(A, mid+1, high);
             else 
-                ind = Ricerca(A,low,mid);
-                if(ind < 0)
-                    return Ricerca(A, mid+1, high);
-                else 
-                    return ind;
-        }
+                return ind;
+    }
 }
 
 int main(){
@@ -47,15 +50,19 @@ int main(){
 
     A.push_back(elem);*/
 
-    int A[] = {50, 20, 18, 13, 12, 21};
+    int A[] = {20, 15, 13, 12, 21};
     int i;
-    i = Ricerca(A, 0, 1);
+    
+
+    i = Ricerca(A, 0, 4);
 
     if(i < 0)
         cout<<"Non esiste soluzione"<<endl<<endl;
     else 
         cout<<endl<<"Indice: "<< i <<endl<< A[i] << " < "<< A[i+1]<<endl<<endl;
 
+
+    //cout<< Ricerca(A, 0, 5) <<endl<<endl;
     return 0;
 }
 
