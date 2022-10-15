921d0707d560c302aea6952d63fab5cab677a10c
Camilla <camillademartino97@gmail.com>
2022-10-15 19:54:15 +0200

    commit

---
+++ ES. MergeSort/MergeSort-Vector/main.cpp
@@ -0,0 +1,34 @@
+/* 
+    Riscrivere il Merge Sort utilizzando il contenitore Vector
+    ed un iteratore
+*/
+
+#include <iostream>
+#include <vector>
+#include "MergeSort.h"
+
+using namespace std;
+//template <typename Item>
+
+int main(){
+    vector<int> A = {9,7,3,1};
+    typename vector<int>::iterator it;
+
+    cout<<endl;
+    for(it = A.begin(); it != A.end(); it++)
+    {
+        cout<<"["<< *it <<"]";
+    }
+    cout<<endl<<endl;
+
+    Algoritmi<int> :: mergeSort(A, A.begin(), A.end());
+
+    cout<<endl<<"Dopo merge"<<endl;
+    for(it = A.begin(); it != A.end(); it++)
+    {
+        cout<<"["<< *it <<"]";
+    }
+    cout<<endl<<endl;
+
+    return 0;
+}
