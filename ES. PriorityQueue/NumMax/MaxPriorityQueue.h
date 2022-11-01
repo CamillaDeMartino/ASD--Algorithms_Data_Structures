edf005e8923e8d9adfb491230256735f39e3e457
Camilla <camillademartino97@gmail.com>
2022-11-02 00:13:57 +0100

    da aggiustare

---
+++ ES. PriorityQueue/NumMax/MaxPriorityQueue.h
@@ -0,0 +1,75 @@
+#ifndef MAXPRIORITYQUEUE_H
+#define MAXPRIORITYQUEUE_H
+
+#include "MaxHeap.h"
+#include <iostream>
+#include <vector>
+using namespace std;
+
+template<class T>
+class MaxPriorityQueue:public MaxHeap<T>
+{
+private:
+   
+public:
+    MaxPriorityQueue(vector<T>);
+
+    void maxHeapInsert(T);
+    T getMaximum();
+    T extractMax();
+    void decreaseKey(int, T);
+};
+
+template<class T> MaxPriorityQueue<T>::MaxPriorityQueue(vector<T> tree):MaxHeap<T>(tree){
+}
+
+template<class T> void MaxPriorityQueue<T>::maxHeapInsert(T nodo)
+{
+    this->insert(nodo);
+}
+
+template<class T> T MaxPriorityQueue<T>::getMaximum()
+{
+    return this->getTree().at(0);
+}
+
+template<class T> T MaxPriorityQueue<T>::extractMax()
+{
+    if(this->getHeapSize() == 0)
+        return -1;
+    
+    T max = getMaximum();
+    swap(this->tree.at(0), this->tree.at(this->getHeapSize()-1));
+    this->setHeapSize(this->getHeapSize()-1);
+    this->maxHeapify(0);
+
+    return max;
+} 
+
+template<class T> void MaxPriorityQueue<T>::decreaseKey(int i, T key)
+{
+    if(key > this->getTree().at(i))
+    {
+        cout<<"Invalid data";
+        exit;
+    }   
+
+    this->tree.at(i) = key;
+
+    this->buildMaxHeap();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
--- ES. PriorityQueue/NumMax/main.cpp
+++ ES. PriorityQueue/NumMax/main.cpp
@@ -5,3 +5,57 @@
                 per k=2 maxnum=5(5,3,6,2,1), 
                 per k=3 maxnum=6(5,4,3,6,2,1)
 */
+
+#include "MaxPriorityQueue.h"
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int main(){
+
+  vector<int> arr{5, 4, 4, 3, 4, 6, 2, 1, 2};
+  map<int, int> amap;
+  map<int,int>::iterator it;
+
+  int num, k = 2;
+
+  for(int i = 0; i < arr.size(); i++)
+  {
+    num = arr.at(i);
+    it = amap.find(num);
+
+    if ( it == amap.end())
+      amap.insert(make_pair(num,1));
+    else   
+      amap.at(num)++;
+  }
+
+
+  for(it =amap.begin(); it!=amap.end(); it++)
+        cout << (*it).first << " => " << (*it).second << endl;
+
+  vector<int> a;
+
+  for(it =amap.begin(); it!=amap.end(); it++)
+        a.push_back((*it).second);
+  
+
+  MaxPriorityQueue<int> tree = MaxPriorityQueue<int>(a);
+  tree.buildMaxHeap();
+  tree.printArray();
+
+  for(int i = 0; i< k; i++)
+  {
+    cout<<tree.getMaximum();
+    tree.decreaseKey(0,tree.getMaximum()-1);
+
+    //tree.buildMaxHeap();
+    tree.printArray(); 
+  }
+
+
+  
+  return 0;
+}
