7a96b1e49ad08299de2ecdbb7be9018ab43922ad
Camilla <camillademartino97@gmail.com>
2022-11-13 14:00:55 +0100

    aggiustare caso base

--- ES. Albero/SumNodes/main.cpp
+++ ES. Albero/SumNodes/main.cpp
@@ -40,15 +40,30 @@ T  minCouple(vector<T> array)
 
 template<class T>
 T  minCoupleRic(vector<T> array ,int i, int diff) 
-{   
+{   cout<<"i = "<< i << endl;
+    cout<<"A[i] = "<<array.at(i)<<endl;
+    cout<<"A[i + 1] = "<<array.at(i+1)<<endl;
+    cout<<"differenza = "<<abs(array.at(i) - array.at(i+1))<<endl;
+    
     int min;
-    if(i == array.size() - 1 )
+    if(i == array.size() - 2)
         return min;
     
-    if(abs(array.at(i) - array.at(i+1)) < diff)
+    if(abs(array.at(i) - array.at(i+1)) <= diff)
+    {   
+        cout<<"if"<<endl;
+        
         min = i;
-
-    return minCoupleRic(array, i+1, abs(array.at(i) - array.at(i+1)));
+        cout<<"min = "<<min<<endl;
+        diff = abs(array.at(i) - array.at(i+1));
+        cout<<"diff = "<<diff<<endl;
+        return minCoupleRic(array, i+1, diff);
+    }    
+    else
+        cout<<"else"<<endl;
+    {   cout<<"diff = "<<diff<<endl;
+        return minCoupleRic(array, i+1, diff);
+    }
          
     
 }
@@ -60,7 +75,7 @@ int main(){
     ABR.insert(15);
     ABR.insert(13);
     ABR.insert(16);
-    ABR.insert(10);
+    ABR.insert(9);
     ABR.insert(11);
     ABR.insert(14);
     ABR.insert(17);
@@ -80,10 +95,10 @@ int main(){
 
     int figlio = minCouple(arr);
 
-    cout<<endl<<"I nodi minimi sono: "<<arr.at(figlio)<<" e "<<arr.at(figlio+1)<<endl;
+    //cout<<endl<<"I nodi minimi sono: "<<arr.at(figlio)<<" e "<<arr.at(figlio+1)<<endl;
     
     int ind = minCoupleRic(arr, 0, UINT16_MAX);
-    cout<<endl<<"I nodi minimi sono: "<<arr.at(ind)<<" e "<<arr.at(ind+1)<<endl;
+    cout<<endl<<"I nodi minimi sono: "<<arr.at(ind)<<" e "<<arr.at(ind-1)<<endl;
     
     return 0;
 }
