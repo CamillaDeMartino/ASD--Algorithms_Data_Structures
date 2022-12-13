c9c52bce7fb36690416f67fbecd9d5bb03a52a22
Camilla <camillademartino97@gmail.com>
2022-12-14 00:49:51 +0100

    commit

--- Prova Impronta/File/main.cpp
+++ Prova Impronta/File/main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <sstream>
 
 using namespace std;
 
@@ -28,24 +29,52 @@ int main(){
 
     for(auto i:vect)
         cout<<i<<" ";
-    
+    cout<<endl;
     
 
     cout<<endl<<"***FILE CON 2 NUMERI PER RIGA***"<<endl;
     string file3 = "myFile2";
     fstream myFile2;
-    char num;
+    string line;
     myFile2.open(file3.c_str(), ios::in);
     
+
+    cout<<"**leggo una specifica linea**"<<endl;
+    int current_line = 0, line_number;
+
+    cout<<endl<<"Che linea vuoi leggere ?"<<endl;
+    cin>>line_number;
     while(!myFile2.eof())
     {
-        myFile2.get(num);
-        cout<<num;
+        current_line++;
+        getline(myFile2, line);
+        if( current_line == line_number) break;
+
+    }
+    cout<<line;
+
+
+
+    string file4 = "myFile2";
+    fstream myFile3;
+    string column;
+    myFile3.open(file3.c_str(), ios::in);
+    cout<<endl<<"**leggo le colonne**"<<endl;
+    while(getline(myFile3, column))
+    {
+        istringstream iss(column);
+
+        int column1, column2;
+        iss>>column1>>column2;
+        cout<<column2<<endl;
     }
 
+     
 
     myFile.close();
     output.close();
     myFile2.close();
+    myFile3.close();
+    
     return 0;
 }
