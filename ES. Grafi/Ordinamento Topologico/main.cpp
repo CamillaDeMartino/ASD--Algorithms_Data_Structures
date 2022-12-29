9e284c78b054fae7790b6cf9261126cc10f3d59e
Camilla <camillademartino97@gmail.com>
2022-12-29 11:32:59 +0100

    commit

--- ES. Grafi/Ordinamento Topologico/main.cpp
+++ ES. Grafi/Ordinamento Topologico/main.cpp
@@ -14,7 +14,7 @@ int main(){
 
     Grafo<string> grafo;
 
-    Vertice<string> g("G");
+    /*Vertice<string> g("G");
     Vertice<string> h("H");
     Vertice<string> i("I");
 
@@ -23,10 +23,30 @@ int main(){
     grafo.addNodo(Nodo<string>(&i));
 
     grafo.addArco(0, &h);         
-    grafo.addArco(1, &i);         
+    grafo.addArco(1, &i);*/
 
+    Vertice<string> shirt("shirt");
+    Vertice<string> tie("tie");
+    Vertice<string> jacket("jacket");
+    Vertice<string> belt("belt");
+    Vertice<string> pants("pants");
+    Vertice<string> undershorts("undershorts");
+
+    grafo.addNodo(Nodo<string>(&shirt));            
+    grafo.addNodo(Nodo<string>(&tie));
+    grafo.addNodo(Nodo<string>(&jacket));
+    grafo.addNodo(Nodo<string>(&belt));            
+    grafo.addNodo(Nodo<string>(&pants));
+    grafo.addNodo(Nodo<string>(&undershorts));
+
+    grafo.addArco(0, &tie);         
+    grafo.addArco(0, &belt);
+    grafo.addArco(1, &jacket);         
+    grafo.addArco(3, &jacket);
+    grafo.addArco(4, &belt);         
+    grafo.addArco(5, &pants);
+    
     grafo.ordinamentoTopologico();
-    //cout<<grafo;
 
     return 0;
 }
